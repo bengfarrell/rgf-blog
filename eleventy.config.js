affb5466d71734dc4e42490ec7e7a1cb06966593
Ben Farrell <ben@benfarrell.com>
2024-07-22 20:12:05 -0700

    add title/tags/categories to page find index

--- eleventy.config.js
+++ eleventy.config.js
@@ -152,6 +152,10 @@ module.exports = function(eleventyConfig) {
 		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
 	});
 
+	eleventyConfig.addFilter("filterCategoryList", function filterCategoryList(cats) {
+		return (cats || []).filter(cat => ["categories"].indexOf(cat) === -1);
+	});
+
 	// Customize Markdown library settings:
 	eleventyConfig.amendLibrary("md", mdLib => {
 		mdLib.use(markdownItAnchor, {
